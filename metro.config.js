8b39b7d453b303a393738eb5b881ecc6cc8f3f5e
RichterSergey <91394458+RichterSergey@users.noreply.github.com>
2025-06-14 13:12:46 +0200

    1

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,21 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const {
+    resolver: { sourceExts, assetExts }
+  } = defaultConfig;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const customConfig = {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  };
+
+  return mergeConfig(defaultConfig, customConfig);
+})();
